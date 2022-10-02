70de327be6f610251f9d2e6990b57d3ee721f0a3
FaneNohman <kingofthepixels1@gmail.com>
2022-10-02 23:01:42 +0600

    third commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 import ClassComponent from './components/ClassComponent';
 import  FunctionComponent  from './components/FunctionComponent';
+import Home from './components/Home';
 function App() {
   const alertValue=(value)=>{
     window.alert(value);
   }
   return (
     <div className="App">
-      <ClassComponent str="class component" num="" func={alertValue}/>
-      <FunctionComponent str="function componetn" num="10" func={alertValue}/>
+      <Router>
+      <nav>
+        <Link to="/" className="link">HOME</Link>
+        <Link to="/class" className="link">CLASS</Link>
+      </nav>
+      <Routes>
+        <Route path="/class" element={<ClassComponent str="class component" num="" func={alertValue}/>}>
+          <Route path='func' element={<FunctionComponent str="class component" num="" func={alertValue}/>}/>
+        </Route>
+        <Route path="/" element={<Home/>} />
+      </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/ClassComponent.jsx
+++ src/components/ClassComponent.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link,Outlet} from "react-router-dom";
 import Form from "./Form";
 
 class ClassComponent extends React.Component{
@@ -7,12 +8,16 @@ class ClassComponent extends React.Component{
     }
     render(){
         return(
-            <div className="class-component">
-                <h1>Class Component</h1>
-                <h2>String: {this.props.str||"no string"}</h2>
-                <h2>Number: {this.props.num||"no number"}</h2>
-                <Form func={this.props.func}/>
-            </div>
+                <div className="class-component">
+                    <Link to="func" className="link">FUNC</Link>
+                    <br/>
+                    <h1>Class Component</h1>
+                    <h2>String: {this.props.str||"no string"}</h2>
+                    <h2>Number: {this.props.num||"no number"}</h2>
+                    <Form func={this.props.func}/>
+                    <Outlet className="outlet" />
+                </div>
+            
         )
     }
 }
